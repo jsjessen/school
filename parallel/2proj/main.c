557ad2ba674556bae9ceaf54faba8ee5de509501
James Jessen <jsjessen@gmail.com>
2015-09-28 15:57:13 -0700

    added a bit more generation

--- parallel/2proj/main.c
+++ parallel/2proj/main.c
@@ -46,10 +46,11 @@
 #define ROOT 0
 #define BIGPRIME 2147483647 // (2^32 - 1) = largest prime storable by signed 32bit int
 
-int n, G;
+// n: world is a n by n grid 
+// G: number of generations to simulate
 
 // Get user input n and G
-get_input()
+get_input(int* n, int* G)
 {
     printf("n = ");
     scanf("%d", n);
@@ -58,13 +59,26 @@ get_input()
     scanf("%d", G);
 }
 
+// Precondition: random number generator seeeded
+int get_random_number(int min, int max)
+{
+    int range = max - min;
+    return rand() % range + min;
+}
+
+// Returns true if odd, false if even
+bool check_isOdd(int x)
+{
+    return x & 1;
+}
+
 // Initialize
-void GenerateInitialGoL()
+void GenerateInitialGoL(int n, bool matrix[][])
 {
     // Goal:
     //      - generate and store initial matrix in parallel
     //      - rank i owns rows [i*(n/p) ... (i+1)*(n/p)-1]
-    
+
     // Generate p random numbers in interval [1 to BIGPRIME] and distribute ith to rank i
     if(rank = ROOT)
     {
@@ -72,22 +86,23 @@ void GenerateInitialGoL()
         max = BIGPRIME;
         range = max - min;
 
-        srand(time(NULL))
+        srand(time(NULL));
 
         for(i = 0; i < p; i++)
         {
-            int rand_num = rand() % range + min;
+            int rand_num = get_random_number(1, BIGPRIME);
             MPI_Send(&rand_num, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
     }
-    
+
     // Each rank:
+    // Assuming you can send msgs to yourself, otherwise have Root give to itself
     {
         int rand_num;
         int sender;
         MPI_Recv(&rand_num, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &sender); 
 
-        srand(rand_num)
+        srand(rand_num);
 
         // use given number as seed, to randomly generate n/p values [1 to BIGPRIME]
         numLocalCells = n / p;
@@ -96,43 +111,61 @@ void GenerateInitialGoL()
             int rand_num = rand() % range + min;
             MPI_Send(&rand_num, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
+        // (i * n/p) to (i * n/p - 1)
+
 
         // if kth random number is even, then kth cell in local portion of matrix, state=Alive, else state=Dead
-        
-    }
 
+    }
 }
+    
 
 // Run Game of Life
-Simulate() 
+void Simulate() 
 {
     // Run for G generations
-    
+
     // For each gen in G:
     //      Barrier: synch cells to same generation
     //      Each rank i determines states for cells it owns
+    
+    {
+
+        DisplayGoL();
+    }
 }
 
 // Determines whether a cell should be alive or dead next generation
 // Precondition: All cells have access to neighboring entries
-DetermineState(int row, int col)
+bool DetermineState(int row, int col)
 {
     return alive or dead
 }
 
 // Display
-DisplayGoL()
+void DisplayGoL()
 {
     // not practical for large n, so display every x generations
     // where x is decided based on timings for specific input size
     //      n < 10 => x == 1
     //      n > 10000 => x == 1000 
     //      I just made this up but you get the idea
-    
+
     // Gather entire matrix in rank 0
     //      send only alive cells for smaller msgs
     //      ok to not do this, for simplicity
     // Display it
+}
+
+void main()
+{
+    int n;
+    int G;
+
+    get_input(&n, &G);
+
+    GenerateInitialGoL();
+    Simulate();
 }
 
 // Report:
