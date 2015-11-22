f0f5b8b9853c1d65861e17d658f9326130efbdd7
James Jessen <jsjessen@gmail.com>
2015-11-22 03:00:59 -0800

    Added security deter3

---
+++ security/3proj/login.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERNAME='wsu483ak'
+
+#ssh ${USERNAME}@server1
+ssh ${USERNAME}@users.isi.deterlab.net
+#ssh $1.jsjessen-project3.WSUCPTS464.isi.deterlab.net
+
+#----------------------------
+
+#ssh node[A-C].DeterIntro_${USERNAME}.WSUCptsS483.isi.deterlab.net
+
+#ifconfig
+#echo "--------------------------------"
+
+#netstat -rn
+#echo "--------------------------------"
+
+#netstat -tulpn
+#echo "--------------------------------"
+
+#sudo tcpdump -i ${IFACE} -w /users/${USERNAME}/${OUTFILE}.pcap
---
+++ security/3proj/upload.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scp $1 wsu464ag@users.deterlab.net:/users/wsu464ag/
